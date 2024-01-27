e9f5580c39ce399f9ed8ab2ed528ae196e634dde
ismail <ismailogunshina@gmail.com>
2024-01-27 11:59:20 +0800

    chore: installed shadcn button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <h1 className='text-3xl underline font-bold'>Hello world</h1>
+      <Button>Hello</Button>
     </div>
   )
 }
